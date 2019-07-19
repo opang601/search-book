b0f911b62d4f76e68b88670be6990448eeb650dd
오충환 <choh@eduwill.net>
2019-07-19 14:48:52 +0900

    프로젝트 생성

---
+++ src/main/java/com/simple/api/book/BookSearchApiApplication.java
@@ -0,0 +1,13 @@
+package com.simple.api.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSearchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSearchApiApplication.class, args);
+	}
+
+}
