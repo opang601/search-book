92fd367b4cc5fa37d1714d72ee7669e2079225a8
오충환 <opang601@gmail.com>
2019-07-23 01:30:39 +0900

    검색어 순위 스케쥴러 사용

--- src/main/java/com/simple/api/book/web/scheduler/SearchRankScheduler.java
+++ src/main/java/com/simple/api/book/web/scheduler/SearchRankScheduler.java
@@ -16,14 +16,17 @@ import org.springframework.stereotype.Component;
 import com.simple.api.book.common.domain.entity.SearchKeywordEntity;
 import com.simple.api.book.common.domain.entity.SearchRankEntity;
 import com.simple.api.book.common.domain.repository.SearchKeywordRepository;
+import com.simple.api.book.common.domain.repository.SearchRankRepository;
 
 @Component
 public class SearchRankScheduler {
 	private Logger logger = LoggerFactory.getLogger(getClass());
 	
-	static final long delayTime = 1000 * 10; //30초마다 검색테이블에서 순위조회 후 순위 테이블에 저장
+	static final long delayTime = 1000 * 10; //10초마다 검색테이블에서 순위조회 후 순위 테이블에 저장
 	static final int limitRank = 10;		//검색어 순위 갯수
 	
+	@Autowired
+	private SearchRankRepository searchRankRepository;
 	@Autowired
 	private SearchKeywordRepository searchKeywordRepository;
 	
@@ -45,7 +48,7 @@ public class SearchRankScheduler {
 				.map(e -> new SearchRankEntity(e.getKey(), e.getValue())).collect(Collectors.toList());
 		
 		list.stream().limit(limitRank)
-					.forEach(l -> System.out.println("Keyword: "+ l.getSearchKeyword()+", Count: "+ l.getCount()));	
+					.forEach(l -> searchRankRepository.save(l));	
 	      
 	   }
 }
